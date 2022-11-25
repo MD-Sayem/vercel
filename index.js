79a5e46be36db5659d6134943666bd59118445b7
MD-Sayem <mdsayemislam56@gmail.com>
2022-11-25 09:48:02 +0600

    second commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.render('index.html');
+  res.sendFile('index.html');
 });
 
 app.listen(5000, () => {
